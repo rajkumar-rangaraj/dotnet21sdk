d07f09a327ba20161145d084cdd1259ea1f597c6
Rajkumar Rangaraj <rajrang@microsoft.com>
2021-06-28 13:35:01 -0700

    Add project files.

---
+++ netcoreapp21test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace netcoreapp21test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
